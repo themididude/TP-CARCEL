191bac8849024af3c4aed31e231018bd00b04c8f
Tilin-punto-exe <juanmartinraiteri@gmail.com>
2025-11-05 20:06:45 -0300

    ...

--- src/Seguridad.java
+++ src/Seguridad.java
@@ -1,4 +1,7 @@
 public interface Seguridad {
+     boolean Activo = false;
+     String placaPolicial = "";
     String generarReporte(); ///generar un reporte policial, dependiendo del rango cambia
-    void switchActivo(); ////
+    void switchActivo();
+   ;////
 }
