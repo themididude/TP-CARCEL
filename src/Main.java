66b9e4b0769a38d8a9ed65484def9cb808dc82ab
mididude <themididude@gmail.com>
2025-11-08 16:48:20 -0300

    a

--- src/Main.java
+++ src/Main.java
@@ -12,58 +12,7 @@ public class Main {
 
     }
 }
-/*
-    /// ────────────────────────────────────────── ///
 
-
-    /// ────────────────────────────────────────── ///
-    private static void MenuUsuario(Scanner sc)
-    {
-        while(true){
-
-            System.out.println("─────────== MENU USER ==────────");             ////user seria un empleado.
-            System.out.println("1. Ver usuarios");
-            System.out.println("2. Stuff");
-            System.out.println("3. Salir");
-            System.out.println("> ");
-
-            int opcion = sc.nextInt();
-            sc.nextLine(); ///limpiar buffer :v
-
-            switch(opcion){
-                case 1:
-                    UserDB.mostrarUsuarios();
-                    break;
-                case 2:
-                    //no hecho todavia
-                    break;
-                case 3:
-                    System.out.println("Cerrando sesion...");
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    break;
-            }
-
-        }
-    }
-    /// ────────────────────────────────────────── ///
-
-    private static void crearUsuario(Scanner sc) { /// ADMIN-ONLY
-        System.out.print("Nombre de usuario: ");
-        String nuevoNombre = sc.nextLine();
-
-        System.out.print("Password: ");
-        String nuevaPass = sc.nextLine();
-
-        System.out.print("Rol (ADMIN / SEGURIDAD / USER): ");
-        String rolStr = sc.nextLine().toUpperCase();
-
-
-
-        System.out.println("Usuario creado con exito.");
-    }
-}*/
 
 /*⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⢀⣴⡟⢫⡿⢙⣳⣄⣈⡙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
